537c9ec0ed77878c7468c7115e6c0eb5304edb73
fjbruno13 <fjbruno13@gmail.com>
2011-03-15 04:32:06 +0000

    Fixed issue with the robot never returning to the starting position between iterations. Also, added the greedy QLearning algorithm to run using the best values from the QTable.

--- src/MyRobot.java
+++ src/MyRobot.java
@@ -25,6 +25,7 @@ public class MyRobot {
 	
 	JButton qLearning;
 	JButton aStar;
+	JButton qLearningGreedy;
 	JPanel panel;
 	JFrame frame;
 
@@ -35,10 +36,13 @@ public class MyRobot {
 	private final int numIter = 100;
 	
 	private State[][] qTable;
+	private Point startPoint;
 	
 	public MyRobot() {
 		qLearning = new JButton("Q Learning");
+		qLearningGreedy = new JButton("Q Learning Greedy");
 		aStar = new JButton("A* planning");
+		startPoint = new Point();
 		
 		aStar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -52,15 +56,46 @@ public class MyRobot {
             }
         });
 		
+		qLearningGreedy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                qLearningGreedyButtonActionPerformed(evt);
+            }
+        });
+		
 		map = new MapGUI();
 		panel = new JPanel();
 		frame = new JFrame();
 		panel.add(qLearning);
 		panel.add(aStar);
+		panel.add(qLearningGreedy);
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
 //		map.getContentPane().add(panel);
+	}
+	
+	private void qLearningGreedyButtonActionPerformed(ActionEvent evt)
+	{
+		if(map.getMap() == null || map.getMap().length == 0)
+		{
+			System.out.println("Load a map first");
+			return;
+		}
+		
+		boolean goalFound = false;
+		
+		while(!goalFound)
+		{
+			goalFound = greedy();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		map.moveRobot(startPoint.x, startPoint.y, "N");
 	}
 	
 	private void qLearningButtonActionPerformed(ActionEvent evt)
@@ -78,13 +113,17 @@ public class MyRobot {
 		
 		initQTable();
 		
+		boolean goalFound = false;
+		
 		for(int i = 0; i < numIter; i++)
 		{
-			boolean goalFound = false;
+			goalFound = false;
 			while(!goalFound)
 			{
 				goalFound = randomWalk();
 			}
+			
+			map.moveRobot(startPoint.x, startPoint.y, "N");
 		}
 		
 		printQTable();
@@ -101,6 +140,90 @@ public class MyRobot {
 				System.out.println(qTable[i][j]);
 			}
 		}
+	}
+	
+	private boolean greedy()
+	{	
+		int[] robot = map.getRobotLocation();
+		
+		State s = qTable[robot[0]][robot[1]];
+		State next = null;
+		String angle = "";
+		
+		boolean blocked = true;
+		while(blocked)
+		{	
+			double maxValue = s.up;
+			int maxValueIndex = 0;
+			double[] dirArray = new double[4];
+			dirArray[0] = s.up;
+			dirArray[1] = s.down;
+			dirArray[2] = s.left;
+			dirArray[3] = s.right;
+			
+			for(int k = 0; k < 4; k++)
+			{
+				if(dirArray[k] > maxValue)
+				{
+					maxValue = dirArray[k];
+					maxValueIndex = k;
+				}
+			}
+			
+			switch(maxValueIndex)
+			{
+				case UP:
+					if(robot[0] < qTable.length - 1 && !qTable[robot[0]+1][robot[1]].isWall())
+					{
+//								System.out.format("Robot (%1$d, %2$d) moving to (%3$d, %4$d)", robot[0], robot[1], robot[0]+1, robot[1]);
+//								System.out.println("Moving UP");
+						next = qTable[robot[0]+1][robot[1]];
+						angle = "N";
+						s.up = update(s, next, s.up);
+						blocked = false;
+					}
+					break;
+				case DOWN:
+					if(robot[0] > 0 && !qTable[robot[0]-1][robot[1]].isWall())
+					{
+//								System.out.format("Robot (%1$d, %2$d) moving to (%3$d, %4$d)", robot[0], robot[1], robot[0]-1, robot[1]);
+//								System.out.println("Moving DOWN");
+						next = qTable[robot[0]-1][robot[1]];
+						angle = "S";
+						s.down = update(s, next, s.down);
+						blocked = false;
+					}
+					break;
+				case LEFT:
+					if(robot[1] > 0 && !qTable[robot[0]][robot[1]-1].isWall())
+					{
+//								System.out.format("Robot (%1$d, %2$d) moving to (%3$d, %4$d)", robot[0], robot[1], robot[0], robot[1]-1);
+//								System.out.println("Moving LEFT");
+						next = qTable[robot[0]][robot[1]-1];
+						angle = "W";
+						s.left = update(s, next, s.left);
+						blocked = false;
+					}
+					break;
+				case RIGHT:
+					if(robot[1] < qTable[0].length - 1 && !qTable[robot[0]][robot[1]+1].isWall())
+					{
+//								System.out.format("Robot (%1$d, %2$d) moving to (%3$d, %4$d)", robot[0], robot[1], robot[0], robot[1]+1);
+//								System.out.println("Moving RIGHT");
+						angle = "E";
+						next = qTable[robot[0]][robot[1]+1];
+						s.right = update(s, next, s.right);
+						blocked = false;
+					}
+				default:
+					
+					break;
+			}
+		}
+		
+		qTable[s.point.x][s.point.y] = s;
+		map.moveRobot(next.point.x, next.point.y, angle);
+		return next.isGoal();
 	}
 
 	private boolean randomWalk() 
@@ -162,7 +285,6 @@ public class MyRobot {
 						blocked = false;
 					}
 				default:
-					
 					break;
 			}
 		}
@@ -205,6 +327,8 @@ public class MyRobot {
 		}
 		
 		qTable[map.getGoal()[0]][map.getGoal()[1]].setGoal();
+		startPoint.x = map.getRobotLocation()[0];
+		startPoint.y = map.getRobotLocation()[1];
 	}
 	
 	private void aStarButtonActionPerformed(ActionEvent evt)
